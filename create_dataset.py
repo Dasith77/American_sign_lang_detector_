a629991084c2db1d015e7ab5af5231b5b79f611c
Dasith77 <65776391+Dasith77@users.noreply.github.com>
2023-09-13 21:39:22 +0530

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -1,26 +1,28 @@
-import os
+import os # Provides functions for interacting with the operating system, used for navigating directories.
 import mediapipe as mp
 import cv2
-import pickle  # use to save datasets models and info
+import pickle  # serializing and deserializing Python objects.
 
 import matplotlib.pyplot as plt
 from mediapipe.python.solutions import hands
 
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands  # provide functionality to hand tracking
+mp_drawing = mp.solutions.drawing_utils  # provide utility functions for drawing landmarks in image
+mp_drawing_styles = mp.solutions.drawing_styles  # provide predefined solutions for drawnig landmarks
 
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.6)
 
 DATA_DIR = './data'
 
-data = []
-class_lables = []
+data = []  #An empty list to store the landmark data for each image.
+class_lables = [] #An empty list to store the class labels (i.e., directory names) 
 
 for dir_ in os.listdir(DATA_DIR):
     for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
-        data_aux = []
-        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))   #It takes the file path as an argument and returns a NumPy array representing the image.
+        data_aux = []  # contains the x and y coordinate values of the landmarks detected on one particular hand in a
+        # single image.
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))  # It takes the file path as an argument and
+        # returns a NumPy array representing the image.
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         results = hands.process(img_rgb)
@@ -36,6 +38,17 @@ for dir_ in os.listdir(DATA_DIR):
             data.append(data_aux)  # this big array represents our image
             class_lables.append(dir_)
 
+#print(len(data_aux))
+#print(len(data))
+#max_landmarks = max(len(entry) for entry in data)
+
+# Pad the data with zeros to match the maximum shape
+#for entry in data:
+    #while len(entry) < max_landmarks:
+        #entry.extend([0.0, 0.0])  # Assuming landmarks are represented as (x, y) pairs
+
+# Now, you can safely convert data into a NumPy array
 # create file to save all this data
 with open('data.pickle', 'wb') as f:
-    pickle.dump({'data': data, 'labels': class_lables}, f)
+    pickle.dump({'data': data, 'labels': class_lables}, f)  # serializes this dictionary and writes it to the
+    # 'data.pickle' file in binary format. This file will now contain the data and labels in a serialized form.
